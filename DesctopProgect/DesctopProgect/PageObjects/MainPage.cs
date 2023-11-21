f1c7f4bcc8fbc987b3e3b8acbe6ab9a97f59a7b8
k.valder <valder.k@bk.ru>
2023-11-21 11:36:43 +0300

    add project

---
+++ DesctopProgect/DesctopProgect/PageObjects/MainPage.cs
@@ -0,0 +1,26 @@
+﻿using Aquality.WinAppDriver.Elements.Interfaces;
+using Aquality.WinAppDriver.Forms;
+using OpenQA.Selenium;
+
+namespace DesctopAuto.PageObject
+{
+    public class MainPage : Form
+    {
+        private static ITextBox DocumentTextBox;
+
+        public MainPage() : base(By.ClassName("Scintilla"), "Main page")
+        {
+            DocumentTextBox = ElementFactory.GetTextBox(By.Name("Scintilla"), "Document text box");
+        }
+
+        public void DocumentTextFillIn(string text)
+        {
+            DocumentTextBox.SendKeys(text);
+        }
+
+        public string GetDocumentText()
+        {
+            return DocumentTextBox.Text;
+        }
+    }
+}
---
+++ DesctopProgect/DesctopProgect/Steps/MainPageStepDefinitions.cs
@@ -0,0 +1,34 @@
+﻿using Aquality.WinAppDriver.Applications;
+using DesctopAuto.PageObject;
+using DesctopAuto.Utils;
+using OpenQA.Selenium.Remote;
+using TechTalk.SpecFlow;
+
+namespace DesctopAuto.StepDefinitions
+{
+    [Binding]
+    public class MainPageStepDefinitions
+    {
+        private MainPage MainPage => new MainPage();
+
+        [Given(@"app is open")]
+        public void GivenAppIsOpen()
+        {
+            DriverUtils.GetDriver();
+            //AqualityServices.WinAppDriverLauncher.StartWinAppDriverIfRequired();
+            AqualityServices.Application.Launch();
+        }
+
+        [When(@"fill in the document with '(.*)'")]
+        public void WhenFillInTheDocumentWith(string text)
+        {
+            MainPage.DocumentTextFillIn(text);
+        }
+
+        [Then(@"is the document fill in")]
+        public void ThenIsTheDocumentFillIn()
+        {
+            MainPage.GetDocumentText();
+        }
+    }
+}
