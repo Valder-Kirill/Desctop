0fa22194b5e1ba012fe633843d60b3105053b26c
k.valder <valder.k@bk.ru>
2023-11-22 10:25:04 +0300

    Add project

---
+++ SpecFlowProject/SpecFlowProject/Steps/MainPageStepDefinitions.cs
@@ -0,0 +1,29 @@
+﻿using Aquality.WinAppDriver.Applications;
+using SpecFlowProject.PageObjects;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject.Steps
+{
+    [Binding]
+    public class MainPageStepDefinitions
+    {
+        [Given(@"app is open")]
+        public void GivenAppIsOpen()
+        {
+            AqualityServices.WinAppDriverLauncher.StartWinAppDriverIfRequired();
+            AqualityServices.Application.Launch();
+        }
+
+        [When(@"fill in the document with '(.*)'")]
+        public void WhenFillInTheDocumentWith(string text)
+        {
+            new MainPage().DocumentTextFillIn(text);
+        }
+
+        [Then(@"is the document fill in")]
+        public void ThenIsTheDocumentFillIn()
+        {
+            new MainPage().GetDocumentText();
+        }
+    }
+}
